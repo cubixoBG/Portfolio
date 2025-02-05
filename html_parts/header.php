2756c2281765d100a9ab3ff885d31de9cf2a915d
cubixo <cubixo2302@gmail.com>
2025-02-05 15:57:52 +0100

    ajout du CV

--- html_parts/header.php
+++ html_parts/header.php
@@ -1,10 +1,12 @@
 <div>
-    <a href="#"><img src="../img/logo_long.webp" alt="logo"></a>
+    <div class="logo">
+        <a href="#"><img src="../img/logo_long.webp" alt="logo"></a>
+    </div>
     <nav>
         <ul>
             <li><a href="index.php">Accueil</a></li>
-            <li><a href="about.php">Mes Projets</a></li>
-            <li><a href="services.php">Mon CV</a></li>
+            <li><a href="projets.php">Mes Projets</a></li>
+            <li><a href="../img/cv.pdf">Mon CV</a></li>
             <li><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
