20befd0c757a176bc27bcd400889960e7e3dbd64
cubixo <cubixo2302@gmail.com>
2025-02-05 15:11:34 +0100

    debut header avec ajout des differents service du menu

--- html_parts/header.php
+++ html_parts/header.php
@@ -2,8 +2,9 @@
     <a href="#"><img src="../img/logo_long.webp" alt="logo"></a>
     <nav>
         <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About</a></li>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="about.php">Mes Projets</a></li>
+            <li><a href="services.php">Mon CV</a></li>
             <li><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
