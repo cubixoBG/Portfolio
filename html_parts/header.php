4700e267854b8ef27f6fecc768bc0542a3e2e696
cubixo <cubixo2302@gmail.com>
2025-02-05 15:09:32 +0100

    modif de l'image logo + début du header

--- html_parts/header.php
+++ html_parts/header.php
@@ -1,1 +1,10 @@
-<h1>test</h1>
+<div>
+    <a href="#"><img src="../img/logo_long.webp" alt="logo"></a>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+</div>
