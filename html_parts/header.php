fd31e9cd8c2875bb352b8cf2ad6d2145c8dcfbdc
cubixo <cubixo2302@gmail.com>
2025-02-06 15:11:24 +0100

    soulignage perfectionné

--- html_parts/header.php
+++ html_parts/header.php
@@ -5,7 +5,7 @@
     <nav>
         <ul>
             <li><a href="index.php">Accueil</a></li>
-            <li><a href="projets.php">Mes Projets</a></li>
+            <li id="headerProject"><a href="projets.php">Mes Projets</a></li>
             <li><a href="../img/cv.pdf" target="_blank">Mon CV</a></li>
             <li><a href="contact.php">Contact</a></li>
         </ul>
