439948ba8db61e779554164734bc32f5acd95cc4
cubixo <cubixo2302@gmail.com>
2025-02-18 17:43:32 +0100

    début footer

---
+++ html_parts/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div>
+        <h2>Me <span>contacter</span></h2>
+        <div class="flex-center">
+            <div id="contact">
+                <div>
+                    <h3>Mail</h3>
+                    <p>morgan.bonne.pro@gmail.com</p>
+                </div>
+                <div>
+                    <h3>Téléphone</h3>
+                    <p>06 15 67 47 83</p>
+                </div>
+            </div>
+</footer>
