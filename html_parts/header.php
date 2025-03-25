e4d92b8c777d310084bf3d9a9d0b730a95fb48a2
cubixo <cubixo2302@gmail.com>
2025-03-25 17:22:36 +0100

    Modification du chemin relatif du CV et ajout d'une anti selection d'elements sur le slider d'img

--- html_parts/header.php
+++ html_parts/header.php
@@ -6,7 +6,7 @@
         <ul>
             <li><a href="../index.php">Accueil</a></li>
             <li id="headerProject"><a href="../public/projets.php">Mes Projets</a></li>
-            <li><a href="./public/img/cv.pdf" target="_blank">Mon CV</a></li>
+            <li><a href="./img/cv.pdf" target="_blank">Mon CV</a></li>
             <li><a href="../public/contact.php">Contact</a></li>
         </ul>
     </nav>
